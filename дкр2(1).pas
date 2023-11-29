0ec98d853e97d7df72ad1a12321ff21e7b4d0c14
kkasuann2000 <144228315+kkasuann2000@users.noreply.github.com>
2023-11-29 22:04:01 +0300

    Add files via upload

---
+++ дкр2(1).pas
@@ -0,0 +1,16 @@
+﻿var n, i, sum: integer;
+  A: array[1..100] of integer;
+begin
+  sum := 0;
+  write('введите кол-во  элементов в массиве: ');
+  readln(n);
+   writeln('введите элементы массива: ');
+  for i := 1 to n do
+    read(A[i]);
+  for i := 1 to n do
+  begin
+    if (i mod 2 <> 0) then
+      sum := sum + A[i];
+  end;
+  writeln('сумма нечет элементов массива: ', sum);
+end.
---
+++ дкр2(2).pas
@@ -0,0 +1,17 @@
+﻿program zad2;
+var str1, str2:string; i, j, c:integer;
+begin
+  writeln('введите строку');
+  readln(str1);
+  writeln('введите подстроку');
+  readln(str2);
+  for i:= 1 to length(str1)-length(str2) + 1 do
+  begin
+    j := 1;
+    while (j <= length(str2)) and (str1[i + j - 1] = str2[j]) do
+      j := j + 1;
+      if j > length(str2) then
+        c := c + 1;
+    end;
+    writeln('кол во вхождений подстроки ' , str2, ' в главной строке ' , str1,':  ', c);
+  end.
